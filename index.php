9656c14538c99ad04dac6f4872e43eeb38d81f57
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-21 14:53:16 +0200

    Aggiunto foglio di stile

--- index.php
+++ index.php
@@ -9,18 +9,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;1,400&display=swap" rel="stylesheet">
+    <!-- Title -->
     <title>Bad Words</title>
 </head>
 <body>
     
-    <p> <?php echo $paragraph; ?> </p>
-    <h4> Numero di caratteri: <?php echo strlen($paragraph); ?> </h4>
-    <h1> Parola vietata: <?php echo $badword; ?></h1>
+    <main>
+        <h1>PHP BAD-WORDS</h1>
+        <p> <?php echo $paragraph; ?> </p>
+        <h4> Numero di caratteri: <?php echo strlen($paragraph); ?> </h4>
+        <h3> Parola vietata: <span><?php echo $badword; ?></span> </h3>
 
-    <!-- Paragrafo Modificato -->
-    <p> <?php echo str_replace( $badword, '***', $paragraph);?> </p>
-    <h4> Numero di caratteri: <?php echo strlen(str_replace( $badword, '***', $paragraph)); ?> </h4>
+        <!-- Paragrafo Modificato -->
+        <p> <?php echo str_replace( $badword, '***', $paragraph);?> </p>
+        <h4> Numero di caratteri: <?php echo strlen(str_replace( $badword, '***', $paragraph)); ?> </h4>
 
+    </main>
 
 </body>
 </html>
